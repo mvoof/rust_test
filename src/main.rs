e3bfdb9ec5c5bb3fca35a60357d683c0a85084c9
mvoof <137293673+mvoof@users.noreply.github.com>
2024-02-26 00:30:43 +0500

    first commit

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    // addition
+    let sum = 5 + 10;
+    println!("{sum}");
+    let f = false;
+    let t = "T";
+    let r = 'R';
+    let emoji = "❤️";
+
+    let tup: (i32, f64, u8) = (500, 6.4, 1);
+}
